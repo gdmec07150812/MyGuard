9adac9c2fab66f8904894970c053a15dee859696
gdmec07150812 <499042532@qq.com>
2016-12-27 18:21:14 +0800

    jacky

--- app/src/main/java/com/gdmec/jacky/myguard/App.java
+++ app/src/main/java/com/gdmec/jacky/myguard/App.java
@@ -8,7 +8,6 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 
-/*asdf*/
 public class App extends Application {
     @Override
     public void onCreate() {
